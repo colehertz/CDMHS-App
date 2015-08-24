7c36eb90b4f84b4aff09a7370da72e05017fa11b
colehertz <chrh02@gmail.com>
2015-08-24 12:42:30 -0700

    Initial Commit

---
+++ CDMHS-App/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  CDMHS-App
+//
+//  Created by Cole H on 8/24/15.
+//  Copyright (c) 2015 Yep Technologies. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
